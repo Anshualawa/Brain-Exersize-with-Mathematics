d87812f07c7999dcc491659e96eff570b913ab08
AnshuAlawa <alawapappu@gmail.com>
2023-05-09 11:23:16 +0530

    modify PlayGame, login nabigation and gamplaye.js file

--- js/gamePlay.js
+++ js/gamePlay.js
@@ -1,1 +1,16 @@
-$(document).ready(function () { })
+$(document).ready(function () { 
+    // alert("Let's Play Game")
+})
+
+var app = angular.module('myGame',['ui.router']);
+
+Directives('enterGame','./directives/login.html');
+Directives('naviGation','./directives/navigation.html');
+
+
+app.config(function($stateProvider,$urlRouterProvider){});
+
+
+function Directives(divName, tempName) {
+    app.directive(divName, function () { return { templateUrl: tempName } });
+}
