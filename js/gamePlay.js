9c360ac8b87037311fb0a8f758d436d1850ad995
AnshuAlawa <alawapappu@gmail.com>
2023-06-05 17:31:59 +0530

    add card

--- js/gamePlay.js
+++ js/gamePlay.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     // alert("Let's Play Game")
+    card();
 })
 
 var app = angular.module('myGame', ['ui.router']);
@@ -29,4 +30,14 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
 function Directives(divName, tempName) {
     app.directive(divName, function () { return { templateUrl: tempName } });
+}
+
+function card() {
+    $('.card').html('<div class="card" style="width: 18rem;"><img src="..." class="card-img-top" alt="...">'
+        + '< div class= "card-body" >'
+        + '<h5 class="card-title">Card title</h5>'
+        + '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card' + "s content.</p>"
+        + '<a href="#" class="btn btn-primary">Go somewhere</a>'
+        + '</div >'
+        + '</div > ')
 }
